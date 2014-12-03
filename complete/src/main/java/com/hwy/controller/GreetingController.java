a3e7fcd0a463be38d5ce0d8117ae5487b0df4668
hongweiye <hongweiye@baidu.com>
2014-12-03 14:42:20 +0800

    添加动态显示时间效果

--- complete/src/main/java/com/hwy/controller/GreetingController.java
+++ complete/src/main/java/com/hwy/controller/GreetingController.java
@@ -1,6 +1,7 @@
 package com.hwy.controller;
 
 import com.hwy.vo.Greeting;
+import org.joda.time.DateTime;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +11,13 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
+    private static final String template = "Hello, %s! now is %s";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
-                            String.format(template, name));
+                            String.format(template, name,
+                                    DateTime.now()));
     }
 }
